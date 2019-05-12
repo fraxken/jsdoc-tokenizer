857ab91329b98cd18e4a26d8cfaa18dc939854e0
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-05-12 20:50:28 +0200

    test: add some default test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,83 @@
+// Require Node.js Dependencies
+const { EOL } = require("os");
+
 // Require Third-party Dependencies
 const avaTest = require("ava");
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Internal Dependencies
+const { scan, TOKENS } = require("../");
+
+/**
+ * @func cleanStr
+ * @param {!String} str str
+ * @returns {String}
+ */
+function cleanStr(str) {
+    str
+        .replace(/\r?\n|\r/, "")
+        .trim()
+        .normalize();
+}
+
+function assertToken(it, token, cp) {
+    const { value, done } = it.next();
+    if (done || value[0] !== token) {
+        return false;
+    }
+
+    switch (token) {
+        case TOKENS.SYMBOL:
+            return cp === String.fromCharCode(value[1]);
+        case TOKENS.IDENTIFIER:
+        case TOKENS.KEYWORD:
+            return cleanStr(cp) === cleanStr(String.fromCharCode(...value[1]));
+        default:
+            return false;
+    }
+}
+
+avaTest("Parse JSDoc: inline @type (v1)", (assert) => {
+    const buf = Buffer.from("/** @type {String} **/");
+    const it = scan(buf);
+
+    assert.true(assertToken(it, TOKENS.KEYWORD, "@type"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "{"));
+    assert.true(assertToken(it, TOKENS.IDENTIFIER, "String"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "}"));
+    const { done } = it.next();
+    assert.true(done);
+});
+
+avaTest("Parse JSDoc: inline @typedef", (assert) => {
+    const buf = Buffer.from("/** @typedef {Array<number>} name **/");
+    const it = scan(buf);
+
+    assert.true(assertToken(it, TOKENS.KEYWORD, "@typedef"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "{"));
+    assert.true(assertToken(it, TOKENS.IDENTIFIER, "Array<number>"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "}"));
+    assert.true(assertToken(it, TOKENS.IDENTIFIER, "name"));
+    const { done } = it.next();
+    assert.true(done);
+});
+
+avaTest("Parse JSDoc: @typedef", (assert) => {
+    const buf = Buffer.from(`/**
+    @const name
+    @example
+    console.log("hello world!");
+    **/`);
+    const it = scan(buf);
+
+    assert.true(assertToken(it, TOKENS.SYMBOL, "\n"));
+    assert.true(assertToken(it, TOKENS.KEYWORD, "@const"));
+    assert.true(assertToken(it, TOKENS.IDENTIFIER, "name"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "\n"));
+    assert.true(assertToken(it, TOKENS.KEYWORD, "@example"));
+    assert.true(assertToken(it, TOKENS.SYMBOL, "\n"));
+
+    // eslint-disable-next-line
+    assert.true(assertToken(it, TOKENS.IDENTIFIER, "console.log(\"hello world!\");"));
+    const { done } = it.next();
+    assert.true(done);
 });
